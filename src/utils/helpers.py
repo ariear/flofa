09283cf51dded1d1810acd27134b838c4dde72ea
Kaveinz <perkasaegy11@gmail.com>
2025-11-20 09:28:46 +0700

    punya egy

--- src/utils/helpers.py
+++
@@ -1,8 +0,0 @@
-import math
-
-def calculate_distance(obj1_rect, obj2_rect):
-    x1 = obj1_rect.centerx
-    y1 = obj1_rect.centery
-    x2 = obj2_rect.centerx
-    y2 = obj2_rect.centery
-    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
